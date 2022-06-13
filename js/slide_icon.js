fb957559b1953f3a011f8792e898a4f0dbaa006d
SUJI <victoria1230120@gmail.com>
2022-06-13 18:17:00 +0900

    스ㄹ릭전
    슬릭전

--- js/slide_icon.js
+++ js/slide_icon.js
@@ -4,11 +4,10 @@ window.onload = function () {
     var $first; var $last; $img.each(function () {
         // 5px 간격으로 배너 처음 위치 시킴                
         $(this).css("left", bannerLeft);
-        bannerLeft += $(this).width() + 10;
+        bannerLeft += $(this).width() + 5;
         $(this).attr("id", "banner" + (++imgCnt));  // img에 id 속성 추가    
     });
-    if (imgCnt > 2) {
-        //배너 9개 이상이면 이동시킴     
+    if (imgCnt > 2) {   
         last = imgCnt;
         setInterval(function () {
             $img.each(function () {
@@ -19,10 +18,10 @@ window.onload = function () {
             $last = $("#banner" + last);
             if ($first.position().left < -200) {
                 // 제일 앞에 배너 제일 뒤로 옮김                        
-                $first.css("left", $last.position().left + $last.width() + 10);
+                $first.css("left", $last.position().left + $last.width() + 5);
                 first++; last++; if (last > imgCnt) { last = 1; } if (first > imgCnt) { first = 1; }
             }
-        }, 50);
+        }, 30);
         //여기 값을 조정하면 속도를 조정할 수 있다.(위에 1px 이동하는 부분도 조정하면 //깔끔하게 변경가능하다           
     }
 };
---
+++ js/slide_prevent.js
@@ -0,0 +1,7 @@
+$('.one_time').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    adaptiveHeight: true
+  });
