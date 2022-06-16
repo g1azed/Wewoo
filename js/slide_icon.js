4345410ea4ec0135dd5ecdf0115aa0261d99b932
SUJI <victoria1230120@gmail.com>
2022-06-17 08:22:55 +0900

    설명 수정
    수정

--- js/slide_icon.js
+++ js/slide_icon.js
@@ -4,7 +4,9 @@ window.onload = function () {
     var last; 
     var imgCnt = 0;
     var $img = $("#icon_slide img");
-    var $first; var $last; $img.each(function () {
+    var $first; 
+    var $last; 
+    $img.each(function () {
         // 5px 간격으로 배너 처음 위치 시킴                
         $(this).css("left", bannerLeft);
         bannerLeft += $(this).width() + 5;
@@ -19,10 +21,12 @@ window.onload = function () {
             });
             $first = $("#banner" + first);
             $last = $("#banner" + last);
-            if ($first.position().left < -200) {
+            if ($first.position().left < 0) {
                 // 제일 앞에 배너 제일 뒤로 옮김                        
                 $first.css("left", $last.position().left + $last.width() + 5);
-                first++; last++; if (last > imgCnt) { last = 1; } if (first > imgCnt) { first = 1; }
+                first++; last++; 
+                if (last > imgCnt) { last = 1; } 
+                if (first > imgCnt) { first = 1; }
             }
         }, 30);
         //여기 값을 조정하면 속도를 조정할 수 있다.(위에 1px 이동하는 부분도 조정하면 //깔끔하게 변경가능하다           
